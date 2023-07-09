5cc3ff7acc59ac908f1fb3d794a2e4e94c24303a
hussein-salemm <husseinsalem177@gmail.com>
2023-07-10 01:09:38 +0300

    splash view ui done

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,8 @@
+import 'package:bookly_app/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/get_navigation.dart';
 
-import 'features/presentation/views/splash_view.dart';
+import 'features/Splash/presentation/views/splash_view.dart';
 
 void main() {
   runApp(const BooklyApp());
@@ -13,8 +14,12 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
-      home: SplashView(),
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData().copyWith(
+        scaffoldBackgroundColor: kPrimaryColor,
+      ),
+      home: const SplashView(),
     );
   }
 }
