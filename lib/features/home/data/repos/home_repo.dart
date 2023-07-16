9cb8a4b87cc8a30174a9e5f9efa56bbf719385ae
hussein-salemm <husseinsalem177@gmail.com>
2023-07-16 22:21:51 +0300

    create home repo methos & failure file

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,5 +1,12 @@
 //Repository pattern (make methods inside abstract class (without implementation))
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo {
-  fetchBestSellerBooks();
-  fetchFeaturedBooks();
+  //Either => (can return 2 types) left side => for errors , right side => return type right
+  //there's many types of errors like (internet or ServerFail or chached and so on ..), so made Failure class
+  // for all errors would happen
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModel>>> fetchFeaturedBooks();
 }
