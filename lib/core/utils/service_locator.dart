a35e4b47c2b2d569d9b9e4a8ebc29fabb9839901
hussein-salemm <husseinsalem177@gmail.com>
2023-07-17 17:00:41 +0300

    setup service locator

---
+++ lib/core/utils/service_locator.dart
@@ -0,0 +1,23 @@
+import 'package:bookly_app/features/home/data/repos/home_repo_impl.dart';
+import 'package:dio/dio.dart';
+import 'package:get_it/get_it.dart';
+
+import 'api_service.dart';
+
+final getIt = GetIt.instance;
+void setupServiceLocator() {
+  //make Singleton (the object is HomeRepoImpl) and make the object between ()
+  getIt.registerSingleton<ApiService>(ApiService(Dio()));
+  getIt.registerSingleton<HomeRepoImpl>(
+    HomeRepoImpl(
+      //get() => return the singelton that i made
+      getIt.get<ApiService>(),
+    ),
+  );
+}
+
+class AuthRepo {
+  final ApiService apiService;
+
+  AuthRepo(this.apiService);
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,18 @@
 import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/core/utils/api_service.dart';
 import 'package:bookly_app/core/utils/app_router.dart';
-import 'package:bookly_app/features/Splash/presentation/views/splash_view.dart';
+import 'package:bookly_app/core/utils/service_locator.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo_impl.dart';
+import 'package:bookly_app/features/home/presentation/manager/featured_books_cubit/featured_books_cubit.dart';
+import 'package:bookly_app/features/home/presentation/manager/newest_books_cubit/newest_books_cubit.dart';
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 //bloc is MVVM (manager is viewModel)
 void main() {
+  setupServiceLocator();
   runApp(const BooklyApp());
 }
 
@@ -16,14 +22,28 @@ class BooklyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp.router(
-      routerConfig: AppRouter.router,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.dark().copyWith(
-        scaffoldBackgroundColor: kPrimaryColor,
-        //to let it know that i'm using dark mode
-        textTheme: GoogleFonts.montserratTextTheme(
-          ThemeData.dark().textTheme,
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) => FeaturedBooksCubit(
+            getIt.get<HomeRepoImpl>(),
+          ),
+        ),
+        BlocProvider(
+          create: (context) => NewestBooksCubit(
+            getIt.get<HomeRepoImpl>(),
+          ),
+        )
+      ],
+      child: MaterialApp.router(
+        routerConfig: AppRouter.router,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData.dark().copyWith(
+          scaffoldBackgroundColor: kPrimaryColor,
+          //to let it know that i'm using dark mode
+          textTheme: GoogleFonts.montserratTextTheme(
+            ThemeData.dark().textTheme,
+          ),
         ),
       ),
     );
